ee9deec0eecc97e0a13455da7e8bb0a13e53095a
haoquantang <hqtang33@gmail.com>
2023-09-10 18:57:54 +0800

    feat: Added mapping extensions

--- src/Movies.Api/Controllers/MoviesController.cs
+++ src/Movies.Api/Controllers/MoviesController.cs
@@ -19,16 +19,8 @@ public class MoviesController : ControllerBase
     [HttpPost("movies")]
     public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
     {
-        var movie = new Movie
-        {
-            Id = Guid.NewGuid(),
-            Title = request.Title,
-            YearOfRelease = request.YearOfRelease,
-            Genres = request.Genres.ToList()
-        };
-
+        Movie movie = request.MapToMovie();
         await _movieRepository.CreateAsync(movie);
-
         return Created($"/api/movies/{movie.Id}", movie);
     }
 }
