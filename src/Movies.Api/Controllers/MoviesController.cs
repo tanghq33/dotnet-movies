2d5a63bae8797d471972a6f948a9bf940eb46104
haoquantang <hqtang33@gmail.com>
2023-09-14 22:17:01 +0800

    Added MovieService

--- src/Movies.Api/Controllers/MoviesController.cs
+++ src/Movies.Api/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 using Movies.Api.Mapping;
 using Movies.Application.Models;
 using Movies.Application.Repositories;
+using Movies.Application.Services;
 using Movies.Contracts.Requests;
 
 namespace Movies.Api.Controllers;
@@ -9,18 +10,18 @@ namespace Movies.Api.Controllers;
 [ApiController]
 public class MoviesController : ControllerBase
 {
-    private readonly IMovieRepository _movieRepository;
+    private readonly IMovieService _movieService;
 
-    public MoviesController(IMovieRepository movieRepository)
+    public MoviesController(IMovieService movieService)
     {
-        _movieRepository = movieRepository;
+        _movieService = movieService;
     }
 
     [HttpPost(ApiEndpoints.Movies.Create)]
     public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
     {
         Movie movie = request.MapToMovie();
-        await _movieRepository.CreateAsync(movie);
+        await _movieService.CreateAsync(movie);
         return CreatedAtAction(nameof(Get), new { idOrSlug = movie.Id }, movie);
     }
 
@@ -28,8 +29,8 @@ public class MoviesController : ControllerBase
     public async Task<IActionResult> Get([FromRoute] string idOrSlug)
     {
         var movie = Guid.TryParse(idOrSlug, out var id)
-            ? await _movieRepository.GetByIdAsync(id)
-            : await _movieRepository.GetBySlugAsync(idOrSlug);
+            ? await _movieService.GetByIdAsync(id)
+            : await _movieService.GetBySlugAsync(idOrSlug);
 
         if (movie is null)
         {
@@ -44,7 +45,7 @@ public class MoviesController : ControllerBase
     [HttpGet(ApiEndpoints.Movies.GetAll)]
     public async Task<IActionResult> GetAll()
     {
-        var movies = await _movieRepository.GetAllAsync();
+        var movies = await _movieService.GetAllAsync();
         var moviesResponse = movies.MapToResponse();
         return Ok(moviesResponse);
     }
@@ -53,8 +54,8 @@ public class MoviesController : ControllerBase
     public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
     {
         var movie = request.MapToMovie(id);
-        var updated = await _movieRepository.UpdateAsync(movie);
-        if (!updated)
+        var updatedMovie = await _movieService.UpdateAsync(movie);
+        if (updatedMovie is null)
         {
             return NotFound();
         }
@@ -65,7 +66,7 @@ public class MoviesController : ControllerBase
     [HttpDelete(ApiEndpoints.Movies.Delete)]
     public async Task<IActionResult> Delete([FromRoute] Guid id)
     {
-        var deleted = await _movieRepository.DeleteByIdAsync(id);
+        var deleted = await _movieService.DeleteByIdAsync(id);
         if (!deleted)
         {
             return NotFound();
---
+++ src/Movies.Application/Services/MovieService.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Http.Headers;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+
+namespace Movies.Application.Services;
+
+public class MovieService : IMovieService
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MovieService(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    public Task<bool> CreateAsync(Movie movie)
+    {
+        return _movieRepository.CreateAsync(movie);
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id)
+    {
+        return _movieRepository.DeleteByIdAsync(id);
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        return _movieRepository.GetAllAsync();
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id)
+    {
+        return _movieRepository.GetByIdAsync(id);
+    }
+
+    public Task<Movie?> GetBySlugAsync(string slug)
+    {
+        return _movieRepository.GetBySlugAsync(slug);
+    }
+
+    /// <summary>
+    /// Question: Why there is no result check for _movieRepository.UpdateAsync,
+    /// what if the repository failed to update the movie ???
+    /// </summary>
+    /// <param name="movie"></param>
+    /// <returns></returns>
+    public async Task<Movie?> UpdateAsync(Movie movie)
+    {
+        var movieExists = await _movieRepository.ExistsByIdAsync(movie.Id);
+        if (!movieExists)
+        {
+            return null;
+        }
+        await _movieRepository.UpdateAsync(movie);
+        return movie;
+    }
+}
